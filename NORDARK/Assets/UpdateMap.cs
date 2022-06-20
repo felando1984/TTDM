f020643f8ff2f80614e46728f718ea5ef03ea4f8
felando1984 <82582362+felando1984@users.noreply.github.com>
2022-06-20 08:12:30 +0200

    Build 0064
    Redesign the UI

--- NORDARK/Assets/UpdateMap.cs
+++ NORDARK/Assets/UpdateMap.cs
@@ -6,6 +6,7 @@ using Mapbox.Unity.Map;
 public class UpdateMap : MonoBehaviour
 {
     private AbstractMap bg_Mapbox;
+    private GameObject MainMenu;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +17,50 @@ public class UpdateMap : MonoBehaviour
         bg_Mapbox.UpdateMap();
         //62.4676991855481, 6.30334069538369
         //17.45
+
+        initGUIMenu();
+    }
+
+    // Build 0055
+    public void initGUIMenu()
+    {
+        MainMenu = GameObject.Find("Canvas_Mainmenu");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.anyKeyDown)
+        {
+            foreach (KeyCode keyCode in System.Enum.GetValues(typeof(KeyCode)))
+            {
+                if (Input.GetKeyDown(keyCode))
+                {
+                    //Debug.Log(keyCode.ToString());
+                    switch (keyCode)
+                    {
+                        case KeyCode.F1:
+                            // Help
+                            break;
+                        case KeyCode.F2:
+                            // Hide / show menu
+                            MainMenu.SetActive(!MainMenu.active);
+                            break;
+                        case KeyCode.F3:
+                            // Screenshot
+                            break;
+                        case KeyCode.F4:
+                            //
+                            break;
+                        case KeyCode.F5:
+                            // Compute
+                            break;
+                        case KeyCode.F6:
+                            // Save
+                            break;
+                    }
+                }
+            }
+        }
     }
 }
